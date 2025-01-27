64f2e40d1bc58a0b8615902f6d357e189fd4c8d4
Ethan32719 <ee6517@pleasantonusd.net>
2025-01-27 21:32:10 +0000

    I wrote insertion sort so far

--- SelectionSort.java
+++ SelectionSort.java
@@ -4,3 +4,34 @@ public class SelectionSort implements Sorter {
         System.out.println("Selection Sort!!!");
     }
 }
+// Java program for implementation of Selection Sort
+import java.util.Arrays;
+
+class GfG {
+
+    static void selectionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+          
+            // Assume the current position holds
+            // the minimum element
+            int min_idx = i;
+
+            // Iterate through the unsorted portion
+            // to find the actual minimum
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                  
+                    // Update min_idx if a smaller element
+                    // is found
+                    min_idx = j;
+                }
+            }
+
+            // Move minimum element to its
+            // correct position
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;           
+        }
+    }
